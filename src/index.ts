b1367b29e954f90d1ce16993e41ac66f3f8b451e
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-24 14:56:48 +0200

    add addEventListener

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import './styles.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll<HTMLButtonElement>('button');
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      console.log(`Нажата кнопка: "${button.textContent?.trim()}"`);
+    });
+  });
+});
